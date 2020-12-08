0c59cc7a367de6344c5043c07896720702e67d42
Mirco Stöpke <m.stoepke@gmail.com>
2020-12-09 00:19:23 +0100

    UI Upgrades

--- PocherUI/ViewModels/MainWindowViewModel.cs
+++ PocherUI/ViewModels/MainWindowViewModel.cs
@@ -9,10 +9,7 @@ using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Forms;
-using System.Windows.Input;
 
 namespace PocherUI.ViewModels
 {
@@ -21,13 +18,38 @@ namespace PocherUI.ViewModels
         public MainWindowViewModel()
         {
             LoadFiles();
-            DD = Directory.GetCurrentDirectory();
             ChangeCommand = new DelegateCommand(x => Change());
             DownloadCommand = new DelegateCommand(x => StartDownload());
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        #region Properties
+        public List<FileModel> Files { get; set; } = new List<FileModel>();
 
+        private string _dd = Directory.GetCurrentDirectory();
+        public string DD
+        {
+            get { return _dd; }
+            set 
+            { 
+                _dd = value;
+                OnPropertyChanged(nameof(DD));
+            }
+        }
+
+        private bool isDownloading;
+        public bool IsDownloading
+        {
+            get { return isDownloading; }
+            set 
+            { 
+                isDownloading = value;
+                OnPropertyChanged(nameof(IsDownloading));
+            }
+        }
+
+        public DelegateCommand ChangeCommand { get; private set; }
+        public DelegateCommand DownloadCommand { get; set; }
+        #endregion
         private void LoadFiles()
         {
             var client = new WebClient();
@@ -41,37 +63,10 @@ namespace PocherUI.ViewModels
                 var title = Regex.Match(match.Value, @"\d+ - ([\w\süöäß]+)").Value;
                 var url = Regex.Match(match.Value, "https:(.+).mp3").Value;
 
-                if(!string.IsNullOrWhiteSpace(title))
+                if (!string.IsNullOrWhiteSpace(title))
                     Files.Add(new FileModel { Title = title, URL = url, Download = false, Status = DownloadStatus.Undefined });
             }
         }
-
-        public List<FileModel> Files { get; set; } = new List<FileModel>();
-
-        private string _dd;
-        public string DD
-        {
-            get { return _dd; }
-            set 
-            { 
-                _dd = value;
-                OnNotifyChanged(nameof(DD));
-            }
-        }
-
-        private bool isDownloading;
-        public bool IsDownloading
-        {
-            get { return isDownloading; }
-            set 
-            { 
-                isDownloading = value;
-                OnNotifyChanged(nameof(IsDownloading));
-            }
-        }
-
-        public DelegateCommand ChangeCommand { get; private set; }
-        public DelegateCommand DownloadCommand { get; set; }
 
         private void Change()
         {
@@ -102,18 +97,17 @@ namespace PocherUI.ViewModels
 
             foreach (var file in downloadList)
             {
-                var currentFile = Files.Where(f => f.Title == file.Title).FirstOrDefault();
-
-                currentFile.Status = DownloadStatus.Loading;
+                file.Status = DownloadStatus.Loading;
                 title = System.IO.Path.Combine(DD, $"{file.Title}.mp3");
                 client.DownloadFile(new Uri(file.URL), title);
-                currentFile.Status = DownloadStatus.Loaded;
+                file.Status = DownloadStatus.Loaded;
             }
 
             IsDownloading = false;
         }
 
-        private void OnNotifyChanged(string propertyName)
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
--- PocherUI/Views/MainWindow.xaml.cs
+++ PocherUI/Views/MainWindow.xaml.cs
@@ -1,25 +1,5 @@
-﻿using Microsoft.Win32;
-using PocherUI.Models;
-using PocherUI.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using PocherUI.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Markup;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PocherUI.Views
 {
